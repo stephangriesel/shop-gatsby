6aa4352a679bad8a25935a6da637912606a7108b
Stephan <sgriesel@gmail.com>
2019-10-29 06:28:56 +0100

    test checout

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,17 +5,30 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = () => {
+  const stripe = window.Stripe("pk_test_uxVeps2FOxzKRvcPqBMCOftw00gMGfkch3")
+  stripe.redirectToCheckout({
+    items: [
+      {
+        sku: "sku_FwQkOSJP53FQWz",
+        quantity: 1
+      }
+    ],
+    successUrl: "http://localhost:8000/",
+    cancelUrl: "http://localhost:8000/"
+  })
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
+  )
+}
 
 export default IndexPage
