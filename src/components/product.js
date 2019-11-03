629bcc224d9da98788440cdc41138aa09710ec7f
Stephan <sgriesel@gmail.com>
2019-11-03 13:51:35 +0100

    add product component

---
+++ src/components/product.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react"
+
+const Product = () => {
+    const stripe = window.Stripe("pk_test_uxVeps2FOxzKRvcPqBMCOftw00gMGfkch3")
+
+    const [sku, setSku] = useState('sku_G6vszZFkytG9wH');
+
+    const placeOrder = () => {
+        stripe.redirectToCheckout({
+            items: [
+                {
+                    sku: sku,
+                    quantity: 1
+                }
+            ],
+            successUrl: "http://localhost:8000/success",
+            cancelUrl: "http://localhost:8000/cancel"
+        })
+    }
+    return (
+        <article>
+            <img src="https://picsum.photos/200/300" alt="Shirt" />
+            <h3>Tanktop</h3>
+            <select value={sku} onChange={(e) => setSku(e.target.value)}>
+                <option value="sku_G6vszZFkytG9wH">S</option>
+                <option value="sku_G6vuNHU9ctlikc">M</option>
+                <option value="sku_FwQkOSJP53FQWz">L</option>
+            </select>
+            <button onClick={placeOrder}>Buy</button>
+        </article>
+    )
+}
+
+export default Product
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,48 +2,13 @@ import React, { useState } from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+import Product from "../components/product"
 
 const IndexPage = () => {
-  const stripe = window.Stripe("pk_test_uxVeps2FOxzKRvcPqBMCOftw00gMGfkch3")
-
-  const [sku, setSku] = useState('sku_G6vszZFkytG9wH');
-
-  const placeOrder = () => {
-    stripe.redirectToCheckout({
-      items: [
-        {
-          sku: sku,
-          quantity: 1
-        }
-      ],
-      successUrl: "http://localhost:8000/success",
-      cancelUrl: "http://localhost:8000/cancel"
-    })
-  }
   return (
     <Layout>
       <div>
-        <article>
-          <img src="https://picsum.photos/200/300" alt="Shirt" />
-          <h3>Haat Oorheers Shirt</h3>
-          <select value={sku} onChange={(e) => setSku(e.target.value)}>
-            <option value="sku_G6vszZFkytG9wH">S</option>
-            <option value="sku_G6vuNHU9ctlikc">M</option>
-            <option value="sku_FwQkOSJP53FQWz">L</option>
-          </select>
-          <button onClick={placeOrder}>Buy</button>
-        </article>
-        <article>
-          <img src="https://picsum.photos/200/300" alt="Shirt" />
-          <h3>Tanktop</h3>
-          <select value={sku} onChange={(e) => setSku(e.target.value)}>
-            <option value="sku_G6vszZFkytG9wH">S</option>
-            <option value="sku_G6vuNHU9ctlikc">M</option>
-            <option value="sku_FwQkOSJP53FQWz">L</option>
-          </select>
-          <button onClick={placeOrder}>Buy</button>
-        </article>
+        <Product />
       </div>
     </Layout>
   )
